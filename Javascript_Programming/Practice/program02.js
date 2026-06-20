464a6861e94a747e19003abec17be52d0b1806dc
AniketDarade2354 <daradeaniket23@gmail.com>
2026-06-21 00:08:36 +0530

    Initial commit

---
+++ Javascript_Programming/Practice/program02.js
@@ -0,0 +1,54 @@
+// Number
+let length = 16;
+let weight = 7.5;
+console.log("lenght is of type : ",typeof length);
+console.log("weight is of type : ",typeof weight);
+
+// BigInt
+let x = 12345678902030304038474637n;
+let y = BigInt(1234747283094727394020394234)
+console.log("x is of type : ",typeof x);
+console.log("y is of type : ",typeof y);
+
+// Strings
+let color = "Yellow";
+let lastname = "Johnson";
+console.log("color is of type : ",typeof color);
+console.log("lastname is of type : ",typeof lastname);
+
+// Boolean
+let a = true;
+let b = false;
+console.log("a is of type : ",typeof a);
+console.log("b is of type : ",typeof b);
+
+// Undefined
+let c;
+let d;
+console.log("c is of type : ",typeof c);
+console.log("d is of type : ",typeof d);
+
+// Null
+let e = null;
+let f = null;
+console.log("e is of type : ",typeof e);
+console.log("f is of type : ",typeof f);
+
+// Symbol
+g = Symbol();
+h = Symbol();
+console.log("g is of type : ",typeof g);
+console.log("h is of type : ",typeof h);
+
+// Object
+person = {firstName:"Jay", lastName:"Ganesh"};
+console.log("person is of type : ",typeof person);
+
+// Array Object
+cars = ["BMW", "Volvo","TAtA"];
+console.log("cars is of type : ",typeof cars);
+
+// Date Object
+date = new Date("2026-06-20");
+console.log("date is of type : ",typeof date);
+
