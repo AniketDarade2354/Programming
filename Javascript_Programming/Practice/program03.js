92c8f0ffd829bfb0a65d6dcff4211c52e51a4fd1
AniketDarade2354 <daradeaniket23@gmail.com>
2026-06-21 00:09:04 +0530

    Initial commit

---
+++ Javascript_Programming/Practice/program03.js
@@ -0,0 +1,14 @@
+
+   let x = 16 + "Volvo";
+// let x = "16" + "Volvo";
+// When adding a number and a string, JavaScript will treat the number as a string.
+
+console.log(x);
+
+x = "Volvo" + 12;
+
+console.log(x);
+
+x = 17 + "Volvo" + 12;
+
+console.log(x);
