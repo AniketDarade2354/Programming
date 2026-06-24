95ff0413a91005214b98532e01893e0c695ba096
AniketDarade2354 <daradeaniket23@gmail.com>
2026-06-24 22:43:02 +0530

    Initial commit

---
+++ Javascript_Programming/Practice/program13.js
@@ -0,0 +1,20 @@
+
+let x = 11;
+let y = 21;
+
+console.log(x += y);    // x = x + y
+
+console.log(x -= y);    // x = x - y
+
+console.log(x *= y);    // x = x * y
+
+console.log(x /= y);    // x = x / y
+
+console.log(x %= y);    // x = x % y
+
+console.log(x **= y);    // x = x ** y
+
+x = "Hello";
+x += " World";
+
+console.log(x);
