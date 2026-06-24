9f8b6099bceab345c5fd556fb06ddb52b436642f
AniketDarade2354 <daradeaniket23@gmail.com>
2026-06-24 22:52:49 +0530

    Initial commit

---
+++ Python_Programming/Practice/program02.py
@@ -0,0 +1,11 @@
+age = 22            # integer
+salary = 50000.50   # float
+grade = 'A'         # charachter
+name = "Aniket"     # string
+passed = True       # boolean
+
+print(age)
+print(salary)
+print(grade)
+print(name)
+print(passed)
