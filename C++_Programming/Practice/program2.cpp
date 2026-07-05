3b887a564e2598c1fd825a6a191d8d494e1323b1
AniketDarade2354 <daradeaniket23@gmail.com>
2026-07-05 23:06:22 +0530

    Initial commit

---
+++ C++_Programming/Practice/program2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j, k;
+
+    cout << "Enter first number : \n";
+    cin >> i;
+
+    cout << "Enter second number : \n";
+    cin >> j;
+
+    k = i + j;
+
+    cout << "Addition is : " << k << "\n";
+
+    return 0;
+}
