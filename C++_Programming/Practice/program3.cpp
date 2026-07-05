4cb2d52a0fb9000ed8c85be6ec46489689ed9b5b
AniketDarade2354 <daradeaniket23@gmail.com>
2026-07-05 23:06:52 +0530

    Initial commit

---
+++ C++_Programming/Practice/program3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Algorithm
+
+    START
+        Accept first number as No1
+        Accept second number as No2
+        Perform addition of No1 & No2
+        Display the result
+    STOP
+*/
+
+int main()
+{
+    float i, j, k;
+
+    cout << "Enter first number : \n";
+    cin >> i;
+
+    cout << "Enter second number : \n";
+    cin >> j;
+
+    k = i + j;
+
+    cout << "Addition is : " << k << "\n";
+
+    return 0;
+}
