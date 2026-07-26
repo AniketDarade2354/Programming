a34c0ee165a6b066adbfdf12e8a3b53b2025b9f0
AniketDarade2354 <daradeaniket23@gmail.com>
2026-07-26 23:41:11 +0530

    Initial commit

---
+++ C++_Programming/Competitive/Assignment17.cpp
@@ -0,0 +1,39 @@
+// Recursive program which accept number from user and return summation of its digits.
+//
+// Input: 879
+// Output: 24
+
+#include <iostream>
+using namespace std;
+
+int Sum(int iNo)
+{
+    static int iSum = 0;
+    int iDigit = 0;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        iSum = iSum + iDigit;
+
+        iNo = iNo / 10;
+
+        Sum(iNo);
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+    
+    iRet = Sum(iValue);
+
+    cout << "Summation of its digits : " << iRet << endl;
+    return 0;
+}
