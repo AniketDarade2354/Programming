1418d14a3c56b1f58d9ddbd6011f4b341710bb38
AniketDarade2354 <daradeaniket23@gmail.com>
2026-08-12 22:49:00 +0530

    initial commit

---
+++ opengl_learning/src/program04.cpp
@@ -0,0 +1,72 @@
+#include <glad/glad.h>      // like GL/gl.h before anything first glad.h
+#include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+    if(NULL == window)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        if(glfwGetKey(window, GLFW_KEY_A))
+        {
+            glClearColor(255.0f, 255.0f, 255.0f, 255.0f); // white screen
+        }
+        else
+        {
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // black screen
+        }
+
+        // render screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // key pressed/released, mouse moved
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, 800, 600);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
