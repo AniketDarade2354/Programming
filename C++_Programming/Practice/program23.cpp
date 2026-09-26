587b5fb52beeb03c1bd8fe19b9ebb0586ffcbafb
AniketDarade2354 <daradeaniket23@gmail.com>
2026-09-26 22:49:54 +0530

    initial commit

---
+++ C++_Programming/Practice/program23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Base {
+public:
+    virtual ~Base() { std::cout << "Base destroyed\n"; }
+};
+
+class Derived : public Base {
+    int* data;
+public:
+    Derived() { data = new int[100]; }
+    ~Derived() { delete[] data; std::cout << "Derived destroyed\n"; }
+};
+
+int main()
+{
+    Base* b = new Derived();
+    delete b;
+}
